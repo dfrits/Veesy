3e2a6037411fb845342814241b5821cc2a6a9e84
TheRooky <fritsch_daniel@gmx.de>
2017-11-08 16:32:51 +0100

    added Dialog-Package mit Alert- und FrageDialog (noch nicht funktionsfähig) TODO Designs erstellen und einpflegen

---
+++ app/src/main/java/de/veesy/dialog/AlertDialog.java
@@ -0,0 +1,49 @@
+package de.veesy.dialog;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import de.veesy.R;
+
+/**
+ * Created by dfritsch on 08.11.2017.
+ * veesy.de
+ * hs-augsburg
+ *
+ * AlertDialog mit beliebigen Titel und einem Bestätigen-Button. Wird der Button gedrückt, wird
+ * der Dialog geschlossen. Darauf kann reagiert werden mit einem OnDissmissListener().
+ */
+
+public class AlertDialog extends DialogFragment implements View.OnClickListener {
+    private static String message;
+
+    /**
+     * Erstellt einen neuen AlertDialog.
+     * @param message Nachricht, die angezeigt werden soll
+     * @return Neuen AlertDialog
+     */
+    static AlertDialog newInstance(String message) {
+        AlertDialog.message = message;
+        return new AlertDialog();
+    }
+
+    @Override
+    public void onClick(View view) {
+        dismiss();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.alert_dialog_wearable, container, false);
+        TextView messageText = view.findViewById(R.id.text1);
+        messageText.setText(message);
+        View button = view.findViewById(R.id.button);
+        button.setOnClickListener(this);
+        return view;
+    }
+}
---
+++ app/src/main/java/de/veesy/dialog/QuestionDialog.java
@@ -0,0 +1,62 @@
+package de.veesy.dialog;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import de.veesy.R;
+
+/**
+ * Created by dfritsch on 08.11.2017.
+ * veesy.de
+ * hs-augsburg
+ */
+
+public class QuestionDialog extends DialogFragment implements View.OnClickListener{
+    private static String message;
+    private static QuestionDialogCallback callback;
+
+    public interface QuestionDialogCallback{
+        void positiveButtonClicked();
+        void negativeButtonClicked();
+    }
+
+    /**
+     * Erstellt einen neuen AlertDialog.
+     * @param message Nachricht, die angezeigt werden soll
+     * @return Neuen AlertDialog
+     */
+    static QuestionDialog newInstance(String message, QuestionDialogCallback callback) {
+        QuestionDialog.message = message;
+        QuestionDialog.callback = callback;
+        return new QuestionDialog();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.button_1) {
+            callback.positiveButtonClicked();
+            dismiss();
+        }
+        else if (view.getId()==R.id.button_2) {
+            callback.negativeButtonClicked();
+            dismiss();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.alert_dialog_wearable, container, false);
+        TextView messageText = view.findViewById(R.id.text1);
+        messageText.setText(message);
+        View button = view.findViewById(R.id.button_1);
+        button.setOnClickListener(this);
+        button = view.findViewById(R.id.button_2);
+        button.setOnClickListener(this);
+        return view;
+    }
+}
