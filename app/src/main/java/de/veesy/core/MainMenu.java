5d202a8467c88f18a940622455a5dd28c53946e5
TheRooky <fritsch_daniel@gmx.de>
2017-11-13 20:24:46 +0100

    edited MainMenu: Nutzer muss 2mal drücken zum Beenden der App

--- app/src/main/java/de/veesy/core/MainMenu.java
+++ app/src/main/java/de/veesy/core/MainMenu.java
@@ -3,9 +3,11 @@ package de.veesy.core;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import de.veesy.R;
 import de.veesy.connection.ConnectionManager;
@@ -16,7 +18,8 @@ import de.veesy.connection.ConnectionManager;
  * hs-augsburg
  */
 public class MainMenu extends Activity {
-
+    // Counter für das Beenden der App
+    private static int backpressed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,7 @@ public class MainMenu extends Activity {
     }
 
     // launching
-    private void initConnectionManager(){
+    private void initConnectionManager() {
         ConnectionManager.instance();
     }
 
@@ -38,7 +41,6 @@ public class MainMenu extends Activity {
      */
     public void bShareClicked(View view) {
         startActivity(new Intent(this, ShareActivity.class));
-        finish();
     }
 
     /**
@@ -55,7 +57,30 @@ public class MainMenu extends Activity {
     public void bSettingsClicked(View view) {
     }
 
+    @Override
+    protected void onPause() {
+        backpressed = 0;
+        super.onPause();
+    }
 
-
-
+    /**
+     * Beendet die App, wenn der Nutzer den Zurück-Button 2mal gedrückt hat. Wurde nach einer
+     * Sekunde der Button nicht zweimal gedrückt, dann wird der Counter zurück gesetzt.
+     */
+    @Override
+    public void onBackPressed() {
+        backpressed++;
+        if (backpressed == 2) {
+            finish();
+        } else {
+            Toast.makeText(this, R.string.backpress_hint, Toast.LENGTH_SHORT).show();
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    backpressed = 0;
+                }
+            }, 1000);
+        }
+    }
 }
