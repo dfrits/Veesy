43dc2a303b6d4fc13c0123405b17deb890ba0b5a
TheRooky <fritsch_daniel@gmx.de>
2017-11-16 17:08:57 +0100

    edited MainMenu: Nutzer mus 2mal drücken um die App zu beenden. Nach einer Sekunde wird der Counter zurück gestetzt.

--- app/src/main/java/de/veesy/core/MainMenu.java
+++ app/src/main/java/de/veesy/core/MainMenu.java
@@ -1,12 +1,13 @@
 package de.veesy.core;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import de.veesy.R;
 import de.veesy.connection.ConnectionManager;
@@ -17,9 +18,9 @@ import de.veesy.connection.ConnectionManager;
  * hs-augsburg
  */
 public class MainMenu extends Activity {
-
     // Counter für das Beenden der App
     private static int counter = 0;
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +37,6 @@ public class MainMenu extends Activity {
 
     /**
      * Aktion des Share-Buttons.
-     *
      * @param view .
      */
     public void bShareClicked(View view) {
@@ -45,7 +45,6 @@ public class MainMenu extends Activity {
 
     /**
      * Aktion des Contacts-Buttons.
-     *
      * @param view .
      */
     public void bContactsClicked(View view) {
@@ -56,7 +55,6 @@ public class MainMenu extends Activity {
 
     /**
      * Aktion des Settings-Buttons.
-     *
      * @param view .
      */
     public void bSettingsClicked(View view) {
@@ -68,6 +66,7 @@ public class MainMenu extends Activity {
     @Override
     protected void onPause() {
         counter = 0;
+        if (timer != null) timer.cancel();
         super.onPause();
     }
 
@@ -80,19 +79,18 @@ public class MainMenu extends Activity {
     }
 
 
-    //TODO super.onDestroy funktioniert aus iwelchen Gründen nicht /:
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
 
-    /**
+    /** TODO Untere Button?
      * Diese Methhode erwartet ein KeyEvent, wenn der untere Button der Uhr gedrückt
      * wird (ID = 265) dann wird des counter bis 5 hochgezählt
      * dann sollte onDestroy() aufgerufen werden, funktioniert allerdings nicht (???)
-     * @param keyCode
-     * @param event
-     * @return
+     * @param keyCode .
+     * @param event .
+     * @return Immer true
      */
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -100,42 +98,18 @@ public class MainMenu extends Activity {
 
         if (keyCode == 265 && event.getAction() == KeyEvent.ACTION_DOWN) {
             counter++;
-            if (counter == 5) {
-                onDestroy();
+            if (counter == 2) {
+                finish();
+            } else {
+                timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        counter = 0;
+                    }
+                }, 1000);
             }
         }
         return true;
     }
-
-
-
-
-    /**
-     *          FUNKTIONIERT NUR IM EMULATOR
-     *
-     * Beendet die App, wenn der Nutzer den Zurück-Button 2mal gedrückt hat. Wurde nach einer
-     * Sekunde der Button nicht zweimal gedrückt, dann wird der Counter zurück gesetzt.
-     */
-//    @Override
-//    public void onBackPressed() {
-//
-//        System.out.println("onBackPressed called...");
-//
-//        backpressed++;
-//        if (backpressed == 2) {
-//            finish();
-//        } else {
-//            Toast.makeText(this, R.string.backpress_hint, Toast.LENGTH_SHORT).show();
-//            Timer timer = new Timer();
-//            timer.schedule(new TimerTask() {
-//                @Override
-//                public void run() {
-//                    backpressed = 0;
-//                }
-//            }, 1000);
-//        }
-//        super.onBackPressed();
-//    }
-
-
 }
