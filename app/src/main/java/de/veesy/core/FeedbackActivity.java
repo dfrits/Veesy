13af7fa105266343f092e32d84f2a83cff5774e0
Gookie <daniel.fritsch1@gmx.de>
2017-11-17 14:52:13 +0100

    edited FeedbackAct: verschiedene Screens werden bei Erfolg/Misserfolg angezeigt

--- app/src/main/java/de/veesy/core/FeedbackActivity.java
+++ app/src/main/java/de/veesy/core/FeedbackActivity.java
@@ -4,10 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-
-import java.util.Observable;
-import java.util.Observer;
 
 import de.veesy.R;
 
@@ -15,59 +11,50 @@ import de.veesy.R;
  * Created by dfritsch on 24.10.2017.
  * veesy.de
  * hs-augsburg
+ * <p><p>
+ * Es muss dem Intent ein BooleanExtra mitgegeben werden.
+ * <p>
+ * Key ist {@link FeedbackActivity#SUCCESS_FLAG}<p>
+ * Wert ist: {@link true} für <b>Erfolg</b> und {@link false} für <b>Misserfolg</b>.
+ * <p>
+ * Wird <b>kein</b> Wert mitgegeben, wird standartmäßig der FailureScreen angezeigt.
  */
-
-/*
- * Wir müssen uns noch überlegen, wie wir der Feedback Activity mitteilen, ob alles geklappt hat. Macht das der ConnectionManager?
- *              ---> Martin: Feedback Activity wird dann über die update Methode von der ShareActivitiy gestartet (vorerst)
- *
- *
- * An sich muss diese Activity gestartet werden und bereits wissen, ob alles funkioniert hat.
- *
- * Dann kann man der ImageView die entsprechende Source zuweißen und somit entweder einen grünen Haken oder ein rotes Kreuz reinladen
- *
- * Falls der ConnectionManager dies macht, dann muss dieser (sobald man sich gepaired hat) die ShareActivity beenden, einen Intent starten,
- * der sozusagen die Animation zu Datenaustausch anzeigt, und sobald dieser vollführt ist, die FeedbackActivity starten
- *
- *              ---> Martin: würde vorerst wie oben beschrieben von der ShareActivity starten
- *              Animation zum Daten austausch könnte man in der ShareActivity über nen anderen Content laden, ähnlihc wie das Radar
- *              is aber nur ein Vorschlag, müsste man noch drüber nachdenken
- *
- */
-public class FeedbackActivity extends Activity implements Observer {
-    public static final String FAILURE = "FAILURE";
-    public static final String SUCCESS = "SUCCESS";
-
-    ImageView feedback_img = null;
+public class FeedbackActivity extends Activity {
+    public static final String SUCCESS_FLAG = "SUCCESS_FLAG";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.feedback_act);
 
-        feedback_img = findViewById(R.id.feedback_imgV);
-        if (getIntent().getStringExtra(FAILURE).equals(FAILURE)) {
-            //feedback_img.setImageDrawable(R.drawable.); TODO X-Bild einfügen
+        if (getIntent().getBooleanExtra(SUCCESS_FLAG, false)) {
+            setContentView(R.layout.feedback_success);
+        } else {
+            setContentView(R.layout.feedback_failure);
         }
-
     }
 
-    protected void onDestroy(){
-        super.onDestroy();
-    }
-
-
+    /**
+     * Schickt den Nutzer zurück zum HomeScreen.
+     * @param view .
+     */
     public void bHomeClicked(View view){
         startActivity(new Intent(this, MainMenu.class));
         finish();
     }
 
+    /**
+     * Zeigt die empfangenen Daten an.
+     * @param view .
+     */
     public void bDetailsClicked(View view){
     }
 
-
-    @Override
-    public void update(Observable observable, Object o) {
-
+    /**
+     * Schickt den Nutzer zurück zur ShareActivity.
+     * @param view .
+     */
+    public void bShareClicked(View view) {
+        startActivity(new Intent(this, ShareActivity.class));
+        finish();
     }
 }
 
