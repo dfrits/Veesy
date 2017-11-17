d15b8837e64e56c66217edf822cfd4af41b6f5f8
Martin Stadlmaier <boarding_division@web.de>
2017-11-17 19:24:50 +0100

    edited ContactActivity

--- app/src/main/java/de/veesy/contacts/ContactActivity.java
+++ app/src/main/java/de/veesy/contacts/ContactActivity.java
@@ -1,6 +1,9 @@
 package de.veesy.contacts;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import de.veesy.R;
 
 /**
  * Created by dfritsch on 24.10.2017.
@@ -9,4 +12,12 @@ import android.app.Activity;
  */
 
 public class ContactActivity extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts);
+    }
 }
+
+
