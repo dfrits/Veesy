3a13b6d41fc248a7d1daa2c2e6b87e66e2c64a2a
Martin Stadlmaier <boarding_division@web.de>
2017-11-17 19:24:33 +0100

    implemented SettingsActivity

---
+++ app/src/main/java/de/veesy/core/SettingsActivity.java
@@ -0,0 +1,18 @@
+package de.veesy.core;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import de.veesy.R;
+
+/**
+ * Created by Martin on 17.11.2017.
+ */
+
+public class SettingsActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.settings);
+    }
+}
