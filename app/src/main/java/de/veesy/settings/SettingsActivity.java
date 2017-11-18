790e9107a1800d39493de0064d0a7423713eb0a6
Martin Stadlmaier <boarding_division@web.de>
2017-11-18 23:01:29 +0100

    edited SettingsActivity

--- app/src/main/java/de/veesy/settings/SettingsActivity.java
+++ app/src/main/java/de/veesy/settings/SettingsActivity.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.veesy.R;
+import de.veesy.connection.ConnectionManager;
 import de.veesy.listview_util.ListItemCallback;
 import de.veesy.listview_util.StraightListAdapter;
 
@@ -21,8 +22,11 @@ import de.veesy.listview_util.StraightListAdapter;
  * hs-augsburg
  */
 
+//TODO ein item "remove all bonded devices" einbauen, funtkion ist momentan in onItemClick();
+
 public class SettingsActivity extends Activity {
     private static List<String> DUMMY_DATA;
+    ConnectionManager connectionManager;
     static {
         DUMMY_DATA = new ArrayList<>();
         DUMMY_DATA.add("My Data");
@@ -36,6 +40,9 @@ public class SettingsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
+        connectionManager = ConnectionManager.instance();
+
+
         initListView();
     }
 
@@ -47,12 +54,17 @@ public class SettingsActivity extends Activity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+                    connectionManager.unpairAllDevices();
             }
         });
     }
 
     public void bShareClicked(View view) {
         finish();
+    }
+
+
+    public void bUnpairClicked(View view){
+
     }
 }
