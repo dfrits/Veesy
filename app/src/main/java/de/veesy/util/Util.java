824530a59bb4d1514db4edcbcf67bd9477a50024
Gookie <daniel.fritsch1@gmx.de>
2017-11-24 00:25:10 +0100

    added Utilklasse

---
+++ app/src/main/java/de/veesy/util/Util.java
@@ -0,0 +1,43 @@
+package de.veesy.util;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+/**
+ * Created by Daniel on 23.11.2017.
+ * veesy.de
+ * hs-augsburg
+ */
+
+public abstract class Util {
+
+    /**
+     * Nimmt den ganzen Schmarn mit runOnUiThread... ab und zeigt einen Toast für eine beliebige
+     * Zeit an.
+     * @param activity       Die aufrufende Acitivity
+     * @param stringResource Text der angezeigt werden soll als String-Resource
+     * @param length         Dauer der Anzeige. {@link android.widget.Toast#LENGTH_SHORT
+     *                       Toast.LENGTH_SHORT} oder {@link android.widget.Toast#LENGTH_LONG
+     *                       Toast.LENGTH_LONG}
+     */
+    public static void showToast(final Activity activity, final int stringResource, final int length) {
+        showToast(activity, activity.getResources().getString(stringResource), length);
+    }
+
+    /**
+     * Die selbe wie {@link Util#showToast(Activity, int, int)}, nur mit einem String statt @string.
+     * @param activity Die aufrufende Acitivity
+     * @param message  Text der angezeigt werden soll als String
+     * @param length   Dauer der Anzeige. {@link android.widget.Toast#LENGTH_SHORT
+     *                 Toast.LENGTH_SHORT} oder {@link android.widget.Toast#LENGTH_LONG
+     *                 Toast.LENGTH_LONG}
+     */
+    public static void showToast(final Activity activity, final String message, final int length) {
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(activity.getApplicationContext(), message, length).show();
+            }
+        });
+    }
+}
